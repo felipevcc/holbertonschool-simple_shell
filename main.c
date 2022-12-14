4a992629b9c5d6598c1dc6a0b0abe20ad45793a9
felipevcc <felipevillamizarc@gmail.com>
2022-12-14 17:06:39 -0500

    project base

--- main.c
+++ main.c
@@ -1,35 +1,32 @@
 #include "main.h"
 
 /**
- * main -
+ * main - open shell, project base
+ * Return: int
 */
+
 int main(void)
 {
 	char *buff = NULL;
-	size_t buff_size;
-	char sep[] = " ";
-	char *buff_args = NULL;
-	int i = 0;
+	size_t buff_size = 0;
+	/*char sep[] = " ";*/
+	/*int i = 0;*/
 
 	while (1)
 	{
-		printf("hsh$ ");	
-		getline(&buff, &buff_size, stdin);
+		if (isatty(0))
+			printf("hsh$ ");
+		buff_size = getline(&buff, &buff_size, stdin);
+		if (buff_size == -1)
+			break;
 
+		buff[buff_size - 1] = '\0';
+		if (execve(buff, NULL, NULL) == -1)
+			perror("Error:");
 
-		while (buff_args[i])
-		{
-			if (i == 0)
-				buff_args[i] = strtok(buff, sep);
-			else
-				buff_args[i] = strtok(NULL, sep);
-			i++;
-		}
-
-		
-
-		printf("%s\n", buff_args);
-		break;
+		if (_strcmp("exit", buff) == 0)
+			break;
+		printf("\n");
 	}
-	return (0);	
+	return (0);
 }
--- string_funcs.c
+++ string_funcs.c
@@ -6,6 +6,7 @@
  * @s2: pointer 2
  * Return: result
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
